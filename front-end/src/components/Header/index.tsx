b64653ac176600a815c178a2802eac59291d7e04
Guilherme <guilherme.marrafon1@gmail.com>
2026-08-09 19:10:22 -0300

    Arrumando plugin tailwindcss

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
     <div className="bg-[#161410]">
-      <div className="w-full md:w-184.25 p-3 md:p-0 mx-auto flex items-center justify-between">
+      <div className="mx-auto flex w-full items-center justify-between p-3 md:w-184.25 md:p-0">
         <img src="./logo.png" alt="" />
-        <div className="bg-[#F2DAAC] w-32.5 flex items-center justify-center rounded-sm cursor-pointer">
+        <div className="flex w-32.5 cursor-pointer items-center justify-center rounded-sm bg-[#F2DAAC]">
           Entrar
         </div>
       </div>
