f76f34779e6560a31d2bd25fb4aeb11c4fcc664b
Guilherme <guilherme.marrafon1@gmail.com>
2026-08-13 08:39:56 -0300

    Estabelecendo conexao com o banco de dados - parte 1

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import { connection } from "./src/db.js";
 
 const app = express();
+connection();
 
 app.get("/", (req, res) => {
-   res.json("Guilherme")
-})
+  res.json("Guilherme");
+});
 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")
+  console.log("Servidor rodando na porta 3000");
 });
